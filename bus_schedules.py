e4e6a56ebcfd6d96f8b641baf21e71d02121ef5a
KPZNET <kpznet@outlook.com>
2021-10-09 08:10:14 -0700

    devs

---
+++ bus_schedules.py
@@ -0,0 +1,112 @@
+bus_sch = {
+  "busstops": [
+    {
+      "name": "pitt",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 09:00",
+          "arrivaltime": "2000-01-31 09:10",
+          "destination": "sfo",
+          "distance": 50
+        },
+        {
+          "departuretime": "2000-01-31 11:00",
+          "arrivaltime": "2000-01-31 13:55",
+          "destination": "oia",
+          "distance": 80
+        }
+      ],
+      "linedistance": 0
+    },
+    {
+      "name": "brentwood",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 06:00",
+          "arrivaltime": "2000-01-31 06:17",
+          "destination": "oak",
+          "distance": 7
+        },
+        {
+          "departuretime": "2000-01-31 06:05",
+          "arrivaltime": "2000-01-31 06:16",
+          "destination": "conc",
+          "distance": 5
+        }
+      ],
+      "linedistance": 25
+    },
+    {
+      "name": "oak",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 06:25",
+          "arrivaltime": "2000-01-31 06:33",
+          "destination": "pitt",
+          "distance": 5
+        }
+      ],
+      "linedistance": 7
+    },
+    {
+      "name": "conc",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 06:30",
+          "arrivaltime": "2000-01-31 06:45",
+          "destination": "ant",
+          "distance": 5
+        },
+        {
+          "departuretime": "2000-01-31 06:28",
+          "arrivaltime": "2000-01-31 06:41",
+          "destination": "clay",
+          "distance": 5
+        },
+        {
+          "departuretime": "2000-01-31 06:40",
+          "arrivaltime": "2000-01-31 06:55",
+          "destination": "orin",
+          "distance": 2
+        }
+      ],
+      "linedistance": 8
+    },
+    {
+      "name": "ant",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 07:00",
+          "arrivaltime": "2000-01-31 07:32",
+          "destination": "pitt",
+          "distance": 3
+        }
+      ],
+      "linedistance": 5
+    },
+    {
+      "name": "clay",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 07:10",
+          "arrivaltime": "2000-01-31 07:20",
+          "destination": "pitt",
+          "distance": 2
+        }
+      ],
+      "linedistance": 4
+    },
+    {
+      "name": "orin",
+      "stops": [
+        {
+          "departuretime": "2000-01-31 07:10",
+          "arrivaltime": "2000-01-31 07:42",
+          "destination": "pitt",
+          "distance": 4
+        }
+      ],
+      "linedistance": 1
+    }
+  ]
+}
